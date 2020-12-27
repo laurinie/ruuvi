b2113d45a3869ce3d1591e1711204223a12fc4cc
laurinie <niemi.late@gmail.com>
2020-12-27 03:01:01 +0200

    Add sort result + Add refresh interval

--- src/components/temperature.tsx
+++ src/components/temperature.tsx
@@ -14,17 +14,45 @@ interface Item {
 export default function Temperature() {
 
     const [latest, setLatest] = useState<Item>()
+    const [refreshInterval, setRefreshInterval] = useState(15000);
+
+    function getData() {
+
+        fetch('https://gqeafe8uxg.execute-api.eu-west-1.amazonaws.com/tags', {
+            method: 'GET'
+        })
+            .then(data => data.json())
+            .then(json => {
+                const sorted = json.Items.sort(function (a: Item, b: Item) {
+                    const at = new Date(a.updated).getTime();
+                    const bt = new Date(b.updated).getTime()
+                    if(at<bt){
+                        return 1
+                    }else if (at>bt){
+                        return -1
+                    }
+                    return 0
+                    
+                })
+                console.log(sorted)
+                setLatest(sorted[0])
+            })
+    }
 
 
     useEffect(() => {
         if (!latest) {
-            fetch('https://gqeafe8uxg.execute-api.eu-west-1.amazonaws.com/tags', {
-                method: 'GET'
-            }).then(data => data.json())
-                .then(json => setLatest(json.Items[0]))
+            getData()
         }
     }, [latest])
 
+    useEffect(() => {
+        console.log("tick")
+        if (refreshInterval && refreshInterval > 0) {
+            const interval = setInterval(getData, refreshInterval);
+            return () => clearInterval(interval);
+        }
+    }, [refreshInterval]);
     return (
         <>
             {latest ?
